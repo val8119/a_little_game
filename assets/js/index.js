ee77767e463ea8576cadaec7a6105d789c422b3d
val8119 <mikk.vahaste@gmail.com>
2020-06-24 19:25:30 +0300

    Very buggy, WIP

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,6 @@
-document.getElementById("add-btn").addEventListener("click", function () {
+todoListItems = ["make the bed", "test task 1", "gay"]
+
+for (var i in todoListItems) {
     var inputBoxValue = document.getElementById("add-input").value;
     console.log(inputBoxValue);
 
@@ -8,17 +10,52 @@ document.getElementById("add-btn").addEventListener("click", function () {
 
     var newListItemText = document.createElement("li");
 
-    newListItem.setAttribute("id", "list-item-text");
+    newListItemText.innerHTML = todoListItems[i];
+
+    newListItemText.setAttribute("id", "list-item-text");
 
     newListItem.appendChild(newListItemText);
 
     var newListItemDelBtn = document.createElement("button");
 
-    newListItem.setAttribute("id", "list-item-del-btn");
+    newListItemDelBtn.innerHTML = "delete";
+
+    newListItemDelBtn.setAttribute("id", "list-item-del-btn");
 
     newListItem.appendChild(newListItemDelBtn);
 
     var list = document.getElementById("list");
 
     list.insertBefore(newListItem, list.lastChild);
+}
+
+document.getElementById("list-item-del-btn").addEventListener("click", function () {
+    this.parentElement.remove();
 });
+
+/*
+document.getElementById("add-btn").addEventListener("click", function () {
+            var inputBoxValue = document.getElementById("add-input").value;
+            console.log(inputBoxValue);
+
+            var newListItem = document.createElement("div");
+
+            newListItem.setAttribute("class", "list-item");
+
+            var newListItemText = document.createElement("li");
+
+            newListItem.setAttribute("id", "list-item-text");
+
+            newListItem.appendChild(newListItemText);
+
+            var newListItemDelBtn = document.createElement("button");
+
+            newListItem.setAttribute("id", "list-item-del-btn");
+
+            newListItem.appendChild(newListItemDelBtn);
+
+            var list = document.getElementById("list");
+
+            list.insertBefore(newListItem, list.lastChild);
+        });
+        */
