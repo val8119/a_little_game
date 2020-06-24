8efe52e0e141a87a1e3787932818b5056d23f595
val8119 <mikk.vahaste@gmail.com>
2020-06-24 21:34:03 +0300

    Created *

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,61 +1,61 @@
-todoListItems = ["make the bed", "test task 1", "gay"]
+// variables
+const container = document.querySelector(".cont");
+const contButtonYes = document.getElementById("START-GAME");
+const contMessage = document.querySelector(".cont-text");
 
-for (var i in todoListItems) {
-    var inputBoxValue = document.getElementById("add-input").value;
-    console.log(inputBoxValue);
+// listeners
+contButtonYes.addEventListener("click", game);
 
-    var newListItem = document.createElement("div");
-
-    newListItem.setAttribute("class", "list-item");
-
-    var newListItemText = document.createElement("li");
-
-    newListItemText.innerHTML = todoListItems[i];
-
-    newListItemText.setAttribute("id", "list-item-text");
-
-    newListItem.appendChild(newListItemText);
-
-    var newListItemDelBtn = document.createElement("button");
-
-    newListItemDelBtn.innerHTML = "delete";
-
-    newListItemDelBtn.setAttribute("id", "list-item-del-btn");
-
-    newListItem.appendChild(newListItemDelBtn);
-
-    var list = document.getElementById("list");
-
-    list.insertBefore(newListItem, list.lastChild);
+// functions
+function createButton(buttonText, color) {
+    var newButton = document.createElement("button");
+    if (color === "normal") {
+        newButton.setAttribute("class", "normal-button")
+    } else if (color === "red") {
+        newButton.setAttribute("class", "red-button")
+    } else if (color === "green") {
+        newButton.setAttribute("class", "green-button");
+    }
+    newButton.setAttribute("id", buttonText);
+    newButton.innerHTML = buttonText;
+    container.appendChild(newButton);
 }
 
-document.getElementById("list-item-del-btn").addEventListener("click", function () {
-    this.parentElement.remove();
-});
+function reloadPage() {
+    location.reload();
+}
 
-/*
-document.getElementById("add-btn").addEventListener("click", function () {
-            var inputBoxValue = document.getElementById("add-input").value;
-            console.log(inputBoxValue);
+// game
+function game() {
+    contButtonYes.remove();
+    contMessage.innerHTML = "Very well. You are in the middle of a room with two doors leading to somewhere. Pick one.";
 
-            var newListItem = document.createElement("div");
+    createButton("LEFT", "normal");
+    createButton("RIGHT", "normal");
 
-            newListItem.setAttribute("class", "list-item");
+    document.getElementById("LEFT").addEventListener("click", function () {
+        document.getElementById("LEFT").remove();
+        document.getElementById("RIGHT").remove();
+        contMessage.innerHTML = "Wrong door. You died from falling into a hole.";
+        createButton("RETRY", "red");
+        document.getElementById("RETRY").addEventListener("click", reloadPage);
+    });
 
-            var newListItemText = document.createElement("li");
-
-            newListItem.setAttribute("id", "list-item-text");
-
-            newListItem.appendChild(newListItemText);
-
-            var newListItemDelBtn = document.createElement("button");
-
-            newListItem.setAttribute("id", "list-item-del-btn");
-
-            newListItem.appendChild(newListItemDelBtn);
-
-            var list = document.getElementById("list");
-
-            list.insertBefore(newListItem, list.lastChild);
+    document.getElementById("RIGHT").addEventListener("click", function () {
+        document.getElementById("LEFT").remove();
+        document.getElementById("RIGHT").remove();
+        contMessage.innerHTML = "You arrive in a pink room. There's a phone on a table. Do you try to call someone?";
+        createButton("YES", "green");
+        createButton("NO", "red");
+        document.getElementById("YES").addEventListener("click", function () {
+            document.getElementById("YES").remove();
+            document.getElementById("NO").remove();
+            contMessage.innerHTML = "Bad luck. The phone doesn't work. Ok. Let's move on. You move towards the other end of the room. There are two doors again.";
         });
-        */
+        document.getElementById("NO").addEventListener("click", function () {
+            document.getElementById("YES").remove();
+            document.getElementById("NO").remove();
+            contMessage.innerHTML = "Ok. Let's move on. You move towards the other end of the room. There are two doors again.";
+        });
+    });
+}
